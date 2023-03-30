a1eb3076a2c9f32be7cd0be71a69624802e2a95d
the-way-to-progress <13852313820@163.com>
2023-03-30 14:17:41 +0800

    11

---
+++ 1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int i;
+    int j;
+    for (i = 0; i < 3;++i)
+        for (j = 0; j < 3; ++j)
+            printf("hh\n");
+    printf("xx\n");
+    return 0;
+}
